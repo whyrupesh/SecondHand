8ed2c5c36dcc33f68cef03ce4ce186869fe97fd6
whyrupesh <srupeshkumar800@gmail.com>
2024-04-07 10:14:23 +0530

    added API in Utils

---
+++ App/Utils/GlobalApi.js
@@ -0,0 +1,27 @@
+import { request, gql } from "graphql-request";
+
+const Master_URL =
+  "https://api-ap-south-1.hygraph.com/v2/cluogldmj1efr07tb2uasenk7/master";
+
+const getProductLists = async () => {
+  const document = gql`
+    query getProductLists {
+      productLists {
+        createdAt
+        description
+        id
+        name
+        price
+        image {
+          url
+        }
+      }
+    }
+  `;
+  const result = await request(Master_URL, query);
+  return result;
+};
+
+export default {
+  getProductLists,
+};
